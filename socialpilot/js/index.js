08e8b722d9381528797f1c9647d1535b89164965
JoelKM <joelkasmor16@gmail.com>
2023-07-27 12:37:18 +0200

    Complete nav bar

---
+++ socialpilot/js/index.js
@@ -0,0 +1,23 @@
+document.querySelectorAll('.link').forEach((listitem) => {
+    listitem.addEventListener('click', () => {
+        console.log("click");
+        let topPosition = document.getElementById(listitem.dataset.page).offsetTop - 100;
+        window.scrollTo({
+            top: topPosition,
+            left: 0,
+            behavior: 'smooth'
+        })
+    });
+})
+
+function displayMobileNav() {
+    let x = document.getElementsByClassName("div-block");
+    let logo = document.getElementById("logo")
+    if (x[0].style.display === "flex") {
+        x[0].style.display = "none";
+        logo.style.display = "flex";
+    } else {
+        x[0].style.display = "flex";
+        logo.style.display = "none";
+    }
+}
